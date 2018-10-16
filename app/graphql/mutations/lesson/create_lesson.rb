65dfc684847598c6e4cbf2884ff34fb641db9c72
seb1441 <sebastienauriault@gmail.com>
2018-10-15 23:29:07 -0400

    Fix create lesson mutation

--- app/graphql/mutations/lesson/create_lesson.rb
+++ app/graphql/mutations/lesson/create_lesson.rb
@@ -8,6 +8,10 @@ class Mutations::Lesson::CreateLesson < Mutations::BaseMutation
     position = (::Level.maximum(:position) || 0) + 1
     title = "Lesson #{position}"
 
+    ::Category.create(title: 'Category 1', position: 1) if ::Category.count == 0
+    ::Level.create(title: 'Level 1', position: 1) if ::Level.count == 0
+    ::Chapter.create(title: 'Chapter 1', position: 1) if ::Chapter.count == 0
+
     new_lesson = ::Lesson.create(title: title, category: ::Category.first, level: ::Level.first, chapter: ::Chapter.first)
     {
       lesson: new_lesson,
